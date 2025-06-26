753fa7dbf408465363b19ff8350c3794d77e5038
3 Moon <tmorriso93@gmail.com>
2025-06-25 23:01:16 -0400

    fix tokens when payment is processed

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,28 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+// import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+// export default handleAuth();
+
+//-------
+
+    import {handleAuth,handleCallback,afterCallback,getSession} from '@auth0/nextjs-auth0';
+    import clientPromise from '../../../lib/mongodb';
+     
+    const authOptions = {
+      async callback(req, res) {
+        try {
+          await handleCallback(req, res, { afterCallback });
+          const { user } = await getSession(req, res);
+          const client = await clientPromise;
+          const db = client.db('wordPilotAi');
+          await db.collection('users').updateOne(
+            { auth0Id: user.sub },
+            { $setOnInsert: { auth0Id: user.sub, availableTokens: 10, },},
+            { upsert: true }
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    };
+     
+    export default handleAuth(authOptions);
