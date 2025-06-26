51928094b5c1c8cce02362af8c62d38db100d71c
3 Moon <tmorriso93@gmail.com>
2025-06-26 17:00:20 -0400

    remove token updates

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,28 +1,28 @@
-// import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-// export default handleAuth();
+export default handleAuth();
 
 //-------
 
-    import {handleAuth,handleCallback,afterCallback,getSession} from '@auth0/nextjs-auth0';
-    import clientPromise from '../../../lib/mongodb';
+    // import {handleAuth,handleCallback,afterCallback,getSession} from '@auth0/nextjs-auth0';
+    // import clientPromise from '../../../lib/mongodb';
      
-    const authOptions = {
-      async callback(req, res) {
-        try {
-          await handleCallback(req, res, { afterCallback });
-          const { user } = await getSession(req, res);
-          const client = await clientPromise;
-          const db = client.db('wordPilotAi');
-          await db.collection('users').updateOne(
-            { auth0Id: user.sub },
-            { $setOnInsert: { auth0Id: user.sub, availableTokens: 10, },},
-            { upsert: true }
-          );
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    };
+    // const authOptions = {
+    //   async callback(req, res) {
+    //     try {
+    //       await handleCallback(req, res, { afterCallback });
+    //       const { user } = await getSession(req, res);
+    //       const client = await clientPromise;
+    //       const db = client.db('wordPilotAi');
+    //       await db.collection('users').updateOne(
+    //         { auth0Id: user.sub },
+    //         { $setOnInsert: { auth0Id: user.sub, availableTokens: 10, },},
+    //         { upsert: true }
+    //       );
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   },
+    // };
      
-    export default handleAuth(authOptions);
+    // export default handleAuth(authOptions);
--- utils/getAppProps.js
+++ utils/getAppProps.js
@@ -1,57 +1,3 @@
-import { getSession } from "@auth0/nextjs-auth0";
-import clientPromise from "../lib/mongodb";
-
-//--
-function isValidObjectId(id) {
-    return typeof id === 'string' && id.match(/^[0-9a-fA-F]{24}$/);
-  }
-//--
-
-export const getAppProps = async (ctx) => {
-    const userSession = await getSession(ctx.req, ctx.res);
-    const client = await clientPromise;
-    const db = client.db("wordPilotAi");
-
-    const user = await db.collection("users").findOne({
-        auth0Id: userSession.user.sub
-    });
-
-    if(!user) {
-        return {
-            availableTokens: 0,
-            posts: [],
-        }
-    }
-
-    const posts = await db
-        .collection("posts")
-        .find({
-        userId: user._id,
-    })
-        .limit(5)
-        .sort({
-        created: -1
-    })
-    .toArray();
-
-    return {
-        availableTokens: user.availableTokens,
-        posts: posts.map(({created, _id, userId, ...rest}) => ({
-            _id: _id.toString(),
-            created: created.toString(),
-            ...rest,
-            // title: post.title,
-            // metaDescription: post.metaDescription,
-            // keywords: post.keywords,
-            // blogPost: post.blogPost
-        })),
-        //   postId: ctx.params?.postId || null,
-        postId: ctx?.params?.postId && isValidObjectId(ctx.params.postId) ? ctx.params.postId : null,
-    };
-};
-
-//  002
-
 // import { getSession } from "@auth0/nextjs-auth0";
 // import clientPromise from "../lib/mongodb";
 
@@ -66,22 +12,16 @@ export const getAppProps = async (ctx) => {
 //     const client = await clientPromise;
 //     const db = client.db("wordPilotAi");
 
-//     let user = await db.collection("users").findOne({
+//     const user = await db.collection("users").findOne({
 //         auth0Id: userSession.user.sub
-//       });
-      
-//       if (!user) {
-//         // Create new user with 25 default tokens
-//         await db.collection("users").insertOne({
-//           auth0Id: userSession.user.sub,
-//           availableTokens: 25,
-//         });
-      
-//         user = {
-//           auth0Id: userSession.user.sub,
-//           availableTokens: 25,
-//         };
-//       }
+//     });
+
+//     if(!user) {
+//         return {
+//             availableTokens: 0,
+//             posts: [],
+//         }
+//     }
 
 //     const posts = await db
 //         .collection("posts")
@@ -109,3 +49,63 @@ export const getAppProps = async (ctx) => {
 //         postId: ctx?.params?.postId && isValidObjectId(ctx.params.postId) ? ctx.params.postId : null,
 //     };
 // };
+
+//  002
+
+import { getSession } from "@auth0/nextjs-auth0";
+import clientPromise from "../lib/mongodb";
+
+//--
+function isValidObjectId(id) {
+    return typeof id === 'string' && id.match(/^[0-9a-fA-F]{24}$/);
+  }
+//--
+
+export const getAppProps = async (ctx) => {
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("wordPilotAi");
+
+    let user = await db.collection("users").findOne({
+        auth0Id: userSession.user.sub
+      });
+      
+      if (!user) {
+        // Create new user with 25 default tokens
+        await db.collection("users").insertOne({
+          auth0Id: userSession.user.sub,
+          availableTokens: 25,
+        });
+      
+        user = {
+          auth0Id: userSession.user.sub,
+          availableTokens: 25,
+        };
+      }
+
+    const posts = await db
+        .collection("posts")
+        .find({
+        userId: user._id,
+    })
+        .limit(5)
+        .sort({
+        created: -1
+    })
+    .toArray();
+
+    return {
+        availableTokens: user.availableTokens,
+        posts: posts.map(({created, _id, userId, ...rest}) => ({
+            _id: _id.toString(),
+            created: created.toString(),
+            ...rest,
+            // title: post.title,
+            // metaDescription: post.metaDescription,
+            // keywords: post.keywords,
+            // blogPost: post.blogPost
+        })),
+        //   postId: ctx.params?.postId || null,
+        postId: ctx?.params?.postId && isValidObjectId(ctx.params.postId) ? ctx.params.postId : null,
+    };
+};
